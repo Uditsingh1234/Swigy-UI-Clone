c6b019cdf51525c76ec04cd35c87468c51c998b2
Udit Singh <141742304+Uditsingh1234@users.noreply.github.com>
2024-08-12 01:22:40 +0530

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,7 +5,7 @@ function Category() {
     const [categories, setCategory] = useState([]);
 
     const fetchCategory = async () => {
-        const response = await fetch("http://localhost:5000/categories")
+        const response = await fetch("https://swigy-ui-clone.onrender.com/categories")
         const data = await response.json();
         setCategory(data);
     }
